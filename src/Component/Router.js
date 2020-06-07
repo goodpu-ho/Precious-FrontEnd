7fd0861778f66085414648ac6f251d7ad3991f27
ho <whdxo10@naver.com>
2020-06-07 18:21:54 +0900

    06/07 Frontend: Header Component : Header Logic part One
    	1. Search에서 입력후 enter시 search page로 이동하게하기.
    	2. 현재 로그인한 내 profile이동하게 하기.
    	• Header가 누가 로그인했는지 알 수 있게 query api를 갖고있어야함.
    	3. Hooks를 사용할수록 header는 점점더 아름다워짐.

--- src/Component/Router.js
+++ src/Component/Router.js
@@ -1,23 +1,30 @@
-import { Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import Feed from "../Routes/Feed";
 import Auth from "../Routes/Auth";
 import React from "react"
+import Profile from "../Routes/Profile";
+import Explore from "../Routes/Explore";
+import Search from "../Routes/Search";
+
 
 const LoggedInRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Feed}></Route>
-  </>
+    <Route path="/explore" component={Explore}></Route>
+    <Route path="/search" component={Search}></Route>
+    <Route path="/:username" component={Profile}></Route>
+  </Switch>
 );
 const LoggedOutRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Auth}></Route>
-  </>
+  </Switch>
 );
 
 const AppRouter = ({ isLoggedIn }) => (
   
-    <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />} </Switch>
+    isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />
   
 );
 
